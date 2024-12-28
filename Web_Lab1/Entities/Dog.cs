ab20e9def88e4d9bed0d431be20c3d00ad341dbd
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2024-12-28 11:51:04 +0200

    Entities were fixed after merge conflict.

--- Web_Lab1/Entities/Dog.cs
+++ Web_Lab1/Entities/Dog.cs
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:Web_Lab1/Entities/Dog.cs
-﻿namespace Web_Lab2.Entities
-========
-﻿namespace DataModel.Entities
->>>>>>>> 89e3a584e324b444283e4a848b999196e7502645:DataModel/Entities/Dog.cs
+namespace Web_Lab2.Entities
 {
     public class Dog
     {
@@ -11,11 +7,7 @@
         public string Breed { get; set; }
         public int Age { get; set; }
         public double Weight { get; set; }
-        public bool IsAvailableForAdoption { get; set;  }
+        public bool IsAvailableForAdoption { get; set; }
         public int? ShelterId { get; set; }
-<<<<<<<< HEAD:Web_Lab1/Entities/Dog.cs
-========
-        public DogShelter? DogShelter { get; set; }
->>>>>>>> 89e3a584e324b444283e4a848b999196e7502645:DataModel/Entities/Dog.cs
     }
 }
--- Web_Lab1/Entities/DogShelter.cs
+++ Web_Lab1/Entities/DogShelter.cs
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:Web_Lab1/Entities/DogShelter.cs
 namespace Web_Lab2.Entities
-========
-namespace DataModel.Entities
->>>>>>>> 89e3a584e324b444283e4a848b999196e7502645:DataModel/Entities/DogShelter.cs
 {
     public class DogShelter
     {
